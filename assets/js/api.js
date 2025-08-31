58594e0e04e416a9cc5f879d85de9a846d11578a
HenriqueSampaio01 <henriqueoccaso.ho@gmail.com>
2025-08-31 19:56:42 -0300

    Arrumando a url

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://github.com/HenriqueSampaio01/portifolio/blob/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/HenriqueSampaio01/portifolio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
