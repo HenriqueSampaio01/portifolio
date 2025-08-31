f15eaeeb29166381270bc0d4aea24c4431ba2cc5
HenriqueSampaio01 <henriqueoccaso.ho@gmail.com>
2025-08-31 19:51:04 -0300

    Editando para aparecer minhas infos

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://github.dev/HenriqueSampaio01/portifolio/blob/main/data/profile.json';
+    const url = 'https://github.com/HenriqueSampaio01/portifolio/blob/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
